65b5f8ae862bb462fd677238a4c8357c8c4f590c
17070610 <schimea@hotmail.com>
2024-12-05 05:18:32 -0800

    Insert left

--- binary_trees.h
+++ binary_trees.h
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -16,3 +19,4 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);

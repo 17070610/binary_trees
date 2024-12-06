3ea53ac7b698775f540dd9a9124199b256cc3c86
17070610 <schimea@hotmail.com>
2024-12-05 23:51:42 -0800

    Balance factor

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,9 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -33,3 +37,5 @@ size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balalance(const binary_tree_t *tree);
+
+#endif
